3a6279cb9e1bd16daa06088e46db4f1966b43281
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2022-06-05 15:46:25 +0200

    animations to grid tiles

--- wordle-game/src/App.js
+++ wordle-game/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [solution, setSolution] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3002/solutions')
+    fetch('http://localhost:3001/solutions')
     .then(res => res.json())
     .then(json => {
       const randomSolution = json[Math.floor(Math.random()*json.length)]
