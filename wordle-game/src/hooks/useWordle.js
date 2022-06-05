a5a28b18e3b31066e74fdecefd991a5c393936b2
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2022-06-05 13:23:55 +0200

    added if-statements to guesses

---
+++ wordle-game/src/hooks/useWordle.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const useWordle = (solution) => {
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([])
+    const [history, setHistory] = useState([])
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    const formatGuess = () => {
+        console.log('formatting the guess - ', currentGuess)
+
+    }
+
+    const addNewGuess = () => {
+
+    }
+
+    const handleKeyup = ({key}) => {
+        if (key === 'Enter') {
+            if (turn > 5 ) {
+                console.log('you used all your guesses')
+                return
+
+            }
+            
+            if (history.includes(currentGuess)) {
+                console.log('you already tried that word')
+                return
+
+            }
+
+            if(currentGuess.length !==5) {
+                console.log('Word must be 5 chars long')
+                return
+
+            }
+            formatGuess()
+        }
+        
+        if(key === 'Backspace') {
+            setCurrentGuess((prev) => {
+                return prev.slice(0, -1)
+
+            })
+            return
+        }
+
+        if (/^[A-Za-z]$/.test(key)) {
+            if (currentGuess.length < 5) {
+                setCurrentGuess((prev) => {
+                    return prev + key
+
+                })
+            }
+        }
+
+    }
+
+    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+
+
+}
+
+export default useWordle
