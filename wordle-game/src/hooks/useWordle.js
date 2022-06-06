cccb37396474793d0a62dd0645ec654c01f63ba8
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2022-06-06 11:24:43 +0200

    colors to keypad

--- wordle-game/src/hooks/useWordle.js
+++ wordle-game/src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)])
     const [history, setHistory] = useState([])
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState ({})
 
     //coloring letters in grey (wrong letter), green (correct) or yellow (right letter at wrong place)
     const formatGuess = () => {
@@ -51,6 +52,29 @@ const useWordle = (solution) => {
         })
         setTurn((prevTurn) => {
             return prevTurn + 1
+        })
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = {...prevUsedKeys}
+            formattedGuess.foreach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if(l.color === 'green'){
+                    newKeys[l.key] = 'green'
+                    return 
+                }
+                if (l.color === 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+
+                if (l.color ==='grey' && currentColor !== 'green' && currentColor !== 'yellow') {
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+            })
+
+            return newKeys
+
         })
         setCurrentGuess('')
 
@@ -99,7 +123,7 @@ const useWordle = (solution) => {
 
     }
 
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
 
 
 }
